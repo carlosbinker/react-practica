aa7d7595d8c991408cea2521581078c6b0f0ffab
Carlos Binker <carlosbinker@gmail.com>
2025-09-21 16:29:45 -0300

    Monto los componentes Item1 e Item2. Es más correcto decir montar que invocar. Al componente Item1 les pasamos las props a través de las claves nombre y precio, que  Item1 las recibe ya con las claves desestructuradas nombre y precio. En cambio en el componente Item2 se recibe un objeto item, al que luego se accede a sus claves mediante . (punto)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css'
 import { Boton } from './components/Boton'
+import { Item1 } from './components/Item1'
+import { Item2 } from './components/Item2'
 
 
 function App() {
+  const prod = { nombre: "remera", precio: 10000 };
  
   return (
     <>
       <div>
         {/* // Autocierro el componente Boton porque no usa children */}
         {/* Le paso las props entre {} al componente Boton*/}
-        <Boton texto={"Aceptar"} color={"green"} /> 
-        <Boton texto={"Cancelar"} color={"red"} / > 
+        <Boton texto={"Aceptar"} color={"green"} />
+        <Boton texto={"Cancelar"} color={"red"} />
+        <Item1 nombre={"pantalon"} precio={5000} />
+        <Item2 item={prod} />
       </div>
-      
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Item1.jsx
@@ -0,0 +1,13 @@
+export const Item1 = ({ nombre, precio }) => {
+    
+    return (
+        
+        <article>
+
+        <p>Item 1</p>
+        <p>{nombre}</p>
+        <p>Precio: $ {precio}</p>
+
+    </article>)
+    
+};
