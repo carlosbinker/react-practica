3aa88c503cb9785b221aab22e5e2f7e45a585bde
Carlos Binker <carlosbinker@gmail.com>
2025-10-14 21:50:18 -0300

    En App.jsx declaramos BrowserRoter envolviendo al div, el header y el footer que quedan fuera del switch, que es Routes, y por último dentro Routes van las rutas con Route, se monta una ruta por cada componente, Con Path indicando la URL y con element le indico qué componente se va a montar para esa URL que puse en path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { ItemlistContainer } from './components/ItemListContainer/ItemListContainer';
 
-
 function App() {
   
+// Incorporo BrowseRouter, Routes (que sería el switch) y Routes
   return (
     <>
-      <div>
-        <Header/> 
-        {/* <ItemlistContainer titulo={"Bienvenidos"}/> */}
-        <ItemDetailContainer/>
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header /> 
+          <Routes>
+            <Route path='/' element={<ItemlistContainer titulo={"Bienvenidos"}/>}/>
+            <Route path='/detail/:id' element={<ItemDetailContainer/>}/>             
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </>
   );
 }
