8490bdc241ece1be5e90efeab16c756e38a4f937
Carlos Binker <carlosbinker@gmail.com>
2025-11-28 17:21:52 -0300

    Soluciono que al seleccionar el filtro se muestre también Bienvenidos al DELIVERY

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
                 <Route path="/" element={<ItemListContainer titulo={"Bienvenidos al DELIVERY"}/>}/>
                 <Route path="/detail/:id" element={<ItemDetailContainer />} />  
                 <Route path="/carrito" element={<Cart />} />
-                <Route path="/category/:category" element={<ItemListContainer titulo={"Bienvenidos"} />} />
+                <Route path="/category/:category" element={<ItemListContainer titulo={"Bienvenidos al DELIVERY"} />} />
               </Route>
                 
               {/* <Route path="/admin" element={<ProductFormContainer />} /> */}
